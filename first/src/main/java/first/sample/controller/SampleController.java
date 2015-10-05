9dc005394f089dd430f68b215411b68cd85f9f6f
const34 <const34@gmail.com>
2015-10-05 15:14:42 +0900

    db connection

--- first/src/main/java/first/sample/controller/SampleController.java
+++ first/src/main/java/first/sample/controller/SampleController.java
@@ -1,20 +1,37 @@
 package first.sample.controller;
 
+import java.util.List;
 import java.util.Map;
+
+import javax.activation.CommandMap;
+import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import first.sample.service.SampleService;
+
 @Controller
 public class SampleController {
     Logger log = Logger.getLogger(this.getClass());
+    
+    @Resource(name="sampleService")
+    private SampleService sampleService;
      
     @RequestMapping(value="/sample/openSampleList.do")
     public ModelAndView openSampleList(Map<String, Object> commandMap) throws Exception{
         ModelAndView mv = new ModelAndView("/test");
-        log.debug("인터셉터 테스트................");
+        return mv;
+    }
+    
+    @RequestMapping(value="/sample/dbTest.do")
+    public ModelAndView openSampleBoardList(Map<String,Object> commandMap) throws Exception{
+        ModelAndView mv = new ModelAndView("/sample/dbTest");
+         
+        List<Map<String,Object>> list = sampleService.selectBoardList(commandMap);
+        mv.addObject("list", list);
          
         return mv;
     }
